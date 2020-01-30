866327970069357a80c34b2f9dc0f840455c54fb
Kenneth Hampton <Kennethhampton1987@gmail.com>
2020-01-29 20:08:06 -0800

    java 1 update 4

--- index.js
+++ index.js
@@ -303,8 +303,13 @@ const argTimesTwo = (num) => {num *2};
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
  */
-function carMaker( /* code here */ ) {
-  /* code here */
+function carMaker(num) {
+const obj = {
+  odometer: num,
+  drive(num){return this.odometer += num}
+}
+return obj;
+
 }
 
 /// ////// END OF CHALLENGE /////////
